3246f6736b23eb01189246ac61e6b451465c4a97
ema-12-martins <a97678@alunos.uminho.pt>
2024-02-22 14:46:32 +0000

    EX1

---
+++ SSI/TP2/cesar.py
@@ -0,0 +1,32 @@
+import sys
+
+def preproc(str):
+      l = []
+      for c in str:
+          if c.isalpha():
+              l.append(c.upper())
+      return "".join(l)  
+
+def main(tipo_operacao,letra_deslocamento,frase):
+    frase=preproc(frase)
+    letra_deslocamento=preproc(letra_deslocamento)
+
+    resultado=""
+    if(tipo_operacao=="enc"):
+        for i in range (len(frase)):
+            novo_char =chr(((ord(frase[i])-65)+(ord(letra_deslocamento)-65)%26)+65)
+            resultado+=novo_char
+        
+    elif(tipo_operacao=="dec"):
+        for i in range (len(frase)):
+            novo_char =chr(((ord(frase[i])-65)-(ord(letra_deslocamento)-65)%26)+65)
+            resultado+=novo_char
+        
+    print(resultado)
+
+if __name__ == "__main__":
+    main(sys.argv[1],sys.argv[2],sys.argv[3])
+
+     
+     
+
