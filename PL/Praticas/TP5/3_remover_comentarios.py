f9b6020964388a039b011c644f46b3d884f3f6b4
ema-12-martins <a97678@alunos.uminho.pt>
2024-03-19 10:39:06 +0000

    TCPs feitos

---
+++ PL/Praticas/TP5/3_remover_comentarios.py
@@ -0,0 +1,67 @@
+import ply.lex as lex
+
+states = (
+    ('comentariobloco', 'exclusive'),
+    ('comentariolinha', 'exclusive')
+)
+
+tokens = (
+    'INICIOBLOCO',
+    'FIMBLOCO',
+    'INICIOLINHA',
+    'FIMLINHA'
+)
+
+def t_INITIAL_INICIOBLOCO(t):
+    r'/\*'
+    lexer.begin('comentariobloco')
+    return t
+
+def t_comentariobloco_FIMBLOCO(t):
+    r'\*/'
+    lexer.begin('INITIAL')
+    return t
+
+def t_INITIAL_INICIOLINHA(t):
+    r'//'
+    lexer.begin('comentariolinha')
+    return t
+
+def t_comentariolinha_FIMLINHA(t):
+    r'\n'
+    lexer.begin('INITIAL')
+    pass  
+
+def t_INITIAL_qualquercoisa(t):
+    r'(.|\n)'
+    t.lexer.texto_ignora_comentario += t.value 
+    print( t.lexer.texto_ignora_comentario)
+
+def t_ANY_error(t):
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+data = '''
+/* comment */ ola1
+
+/* comment****comment */ ola2 /*
+comment
+/* comentário dentro de comentário */
+****/ ola3
+
+/*********/
+
+ola4
+ mais um pouco // remover comentário inline
+FIM
+'''
+
+lexer.texto_ignora_comentario = ''  
+lexer.input(data)
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    print(tok)
