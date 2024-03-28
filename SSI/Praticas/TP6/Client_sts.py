cb28f11df23d6d9e030229d0e55951f76cafaeb5
ema-12-martins <a97678@alunos.uminho.pt>
2024-03-28 14:54:07 +0000

    coisa

--- SSI/Praticas/TP6/Client_sts.py
+++ SSI/Praticas/TP6/Client_sts.py
@@ -1,87 +1,127 @@
-# Código baseado em https://docs.python.org/3.6/library/asyncio-stream.html#tcp-echo-client-using-streams
 import asyncio
-import socket
-
-import funcoes_auxiliares as fa
-
 import secrets
 import os
-from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-from cryptography.hazmat.primitives import hashes
-
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives.asymmetric import dh
-
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-
+from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.serialization import load_der_public_key
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+import myCrypt as mc
+from cryptography import x509
+import random
 
 conn_port = 8442
 max_msg_size = 9999
+
 class Client:
     def __init__(self, sckt=None):
         self.sckt = sckt
         self.msg_cnt = 0
+                
+        #generates a base^exp number, where exp is random
+        self.base = 2
+        self.exp = random.randint(2, 10) 
+        self.gx = self.base**self.exp
 
-        with open("MSG_CLI1.key", "rb") as key_file:
-                self.client_private_key = serialization.load_pem_private_key(
-                    key_file.read(),
-                    password=b"1234",
-            ) 
-        self.client_public_key = self.client_private_key.public_key()
+        with open('MSG_CLI1.key','rb') as key_file:
+            self.clientRSA_private_key = serialization.load_pem_private_key(key_file.read(),password=b"1234")
+        
+        self.clientRSA_public_key = self.clientRSA_private_key.public_key()
+
+        self.client_certificate=mc.cert_load('MSG_CLI1.crt')
+
         
     def process(self, msg=b""):
-        if self.msg_cnt ==0:
-            self.msg_cnt +=1
-            return self.client_public_key.public_bytes(encoding=serialization.Encoding.DER, format=serialization.PublicFormat.SubjectPublicKeyInfo)
+        if self.msg_cnt < 2:
+            return self._process_handshake(msg)
+        else:
+            return self._process_message(msg)
+
+
+    def _process_handshake(self, msg=b""):
+        if self.msg_cnt==0:
+            self.msg_cnt+=1
+            return mc.mkpair(self.clientRSA_public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo),self.gx.to_bytes(1000, 'big'))
         
-        elif self.msg_cnt ==1:
-            self.msg_cnt +=1
+        elif self.msg_cnt==1:
+            self.peerRSA_public_key = load_pem_public_key(mc.unpair(mc.unpair(mc.unpair(msg)[0])[0])[0])
+            server_certificate=x509.load_pem_x509_certificate((mc.unpair(msg)[1]), default_backend())
+            gy = int.from_bytes(mc.unpair(mc.unpair(mc.unpair(msg)[0])[0])[1],'big')
+            self.shared_key = gy**self.exp
 
-            peer_public_key, sign = fa.unpair(msg)
-            self.peer_public_key = load_der_public_key(peer_public_key)
-
-            msg = ""
-            self.shared_key = self.client_private_key.exchange(self.peer_public_key)
-
-            self.derived_key = HKDF(
+            self.aesgcm = AESGCM(HKDF(
                 algorithm=hashes.SHA256(),
                 length=32,
                 salt=None,
                 info=b'handshake data',
-            ).derive(self.shared_key)
-            self.aesgcm = AESGCM(self.derived_key)
+                backend=default_backend()
+            ).derive(self.shared_key.to_bytes(1000, 'big')))
 
-           
-                    
-            signature = self.client_private_key.sign(
-                fa.mkpair(self.client_public_key, self.peer_public_key),
+            # Server Certificate Validation
+            mc.valida_cert(server_certificate)
+            
+
+
+            #Server Signature Validation
+            nonce = mc.unpair(mc.unpair(msg)[0])[1][:12]
+            serverEncryptedSignature = mc.unpair(mc.unpair(msg)[0])[1][12:]
+            aad = b""
+            serverDecryptedSignature = self.aesgcm.decrypt(nonce, serverEncryptedSignature, aad)
+
+            try:
+                self.peerRSA_public_key.verify(
+                    serverDecryptedSignature,
+                    mc.mkpair(gy.to_bytes(1000, 'big'),self.gx.to_bytes(1000, 'big')),
+                    padding.PSS(
+                        mgf=padding.MGF1(hashes.SHA256()),
+                        salt_length=padding.PSS.MAX_LENGTH
+                    ),
+                    hashes.SHA256()
+                )
+            except InvalidSignature:
+                print("Validação da assinatura digital não efetuada!!!")
+                return
+
+            #Client Signature
+            clientSignature = self.clientRSA_private_key.sign(
+                mc.mkpair(self.gx.to_bytes(1000, 'big'),gy.to_bytes(1000, 'big')),
                 padding.PSS(
                     mgf=padding.MGF1(hashes.SHA256()),
                     salt_length=padding.PSS.MAX_LENGTH
                 ),
                 hashes.SHA256()
-            )           
-            return signature
+            )
+   
+            nonce = os.urandom(12) 
+            ct = nonce + self.aesgcm.encrypt(nonce, clientSignature, None)
+            
+            self.msg_cnt+=1
+            return mc.mkpair(ct,self.client_certificate.public_bytes(encoding=serialization.Encoding.PEM))
 
-        
-        self.msg_cnt +=1
 
-        if msg :  
+    def _process_message(self, msg):
+        if self.msg_cnt==2:
+            print(msg.decode())
+
+        elif self.msg_cnt>2:
             nonce = msg[:12]
             informacao = msg[12:]
             aad = b""
-            msg = self.aesgcm.decrypt(nonce, informacao, aad)
-            print("msg: ", msg.decode())
+            decrypted_msg = self.aesgcm.decrypt(nonce, informacao, aad)
+            print("Decrypted message:", decrypted_msg.decode())
 
-
-        print('Input message to send (empty to finish)')
-        nonce = secrets.token_bytes(12)
-        new_msg = bytes(input(),"utf-8")
-        ct = nonce + self.aesgcm.encrypt(nonce, new_msg, b"")
-
-        return ct if len(new_msg)>0 else None
+        print('\nInput message to send (empty to finish)')
+        new_msg = input(">> ")
+        if new_msg:
+            self.msg_cnt +=1
+            nonce = secrets.token_bytes(12)
+            ct = nonce + self.aesgcm.encrypt(nonce, new_msg.encode(), b"")
+            return ct if len(new_msg) > 0 else None
+                
 
 async def tcp_echo_client():
     reader, writer = await asyncio.open_connection('127.0.0.1', conn_port)
@@ -98,6 +138,7 @@ async def tcp_echo_client():
     writer.write(b'\n')
     print('Socket closed!')
     writer.close()
+
 
 def run_client():
     loop = asyncio.get_event_loop()
--- SSI/Praticas/TP6/Server_sts.py
+++ SSI/Praticas/TP6/Server_sts.py
@@ -1,80 +1,127 @@
-# Código baseado em https://docs.python.org/3.6/library/asyncio-stream.html#tcp-echo-client-using-streams
 import asyncio
 import os
-from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-from cryptography.hazmat.primitives import hashes
-import funcoes_auxiliares as fa
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives.asymmetric import dh
-
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-
+from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.serialization import load_der_public_key
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+import myCrypt as mc
+from cryptography import x509
+import random
 
 conn_cnt = 0
 conn_port = 8442
 max_msg_size = 9999
+
 class ServerWorker(object):
     def __init__(self, cnt, addr=None):
         self.id = cnt
         self.addr = addr
         self.msg_cnt = 0
+
+        #generates a base^exp number, where exp is random
+        self.base = 2
+        self.exp = random.randint(2, 10) 
+        self.gy = self.base**self.exp
         
-        with open("MSG_SERVER.key", "rb") as key_file:
-            self.server_private_key = serialization.load_pem_private_key(
-            key_file.read(),
-            password=b"1234",
-        ) 
-        self.server_public_key = self.server_private_key.public_key()
+        with open('MSG_SERVER.key','rb') as key_file:
+            self.serverRSA_private_key = serialization.load_pem_private_key(key_file.read(),password=b"1234")
+
+        
+        self.certificate=mc.cert_load('MSG_SERVER.crt')
+
+        self.serverRSA_public_key = self.serverRSA_private_key.public_key()
 
 
     def process(self, msg):
-        if self.msg_cnt == 0:
-            print(msg)
-            
-            try:
-                self.peer_public_key = load_der_public_key(msg)
-                self.shared_key = self.server_private_key.exchange(self.peer_public_key)
-            except Exception as e:
-                print("Error loading DER public key:", e)
-            self.derived_key = HKDF(
+        if self.msg_cnt < 2:
+            return self._process_handshake(msg)
+        else:
+            return self._process_message(msg)
+
+
+    def _process_handshake(self, msg):
+        if self.msg_cnt==0:
+            self.peerRSA_public_key = load_pem_public_key(mc.unpair(msg)[0])
+            self.peerGX = int.from_bytes(mc.unpair(msg)[1],'big')
+            self.shared_key = self.peerGX**self.exp
+
+            self.aesgcm = AESGCM(HKDF(
                 algorithm=hashes.SHA256(),
                 length=32,
                 salt=None,
                 info=b'handshake data',
-            ).derive(self.shared_key)
-            self.aesgcm = AESGCM(self.derived_key)
+                backend=default_backend()
+            ).derive(self.shared_key.to_bytes(1000, 'big')))
 
-            client_PublicKey = msg
-
-            signature = self.server_private_key.sign(
-                fa.mkpair(self.server_public_key, client_PublicKey),
+            #Server Signature
+            serverSignature = self.serverRSA_private_key.sign(
+                mc.mkpair(self.gy.to_bytes(1000, 'big'),self.peerGX.to_bytes(1000, 'big')),
                 padding.PSS(
                     mgf=padding.MGF1(hashes.SHA256()),
                     salt_length=padding.PSS.MAX_LENGTH
                 ),
                 hashes.SHA256()
-            )           
+            )
+
+            nonce = os.urandom(12) 
+            ct = nonce + self.aesgcm.encrypt(nonce, serverSignature, None)
             
-            send = fa.mkpair(self.server_public_key, signature)
             self.msg_cnt += 1
-            return send, self.msg_cnt
 
-        self.msg_cnt += 1
-        nonce = msg[:12]
-        informacao = msg[12:]
-        aad = b""
-        msg = self.aesgcm.decrypt(nonce, informacao, b"")
-   
-        new_msg = msg.upper()
-        print("new msg: ", new_msg.decode())
+            return mc.mkpair(mc.mkpair(mc.mkpair(self.serverRSA_public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo),self.gy.to_bytes(1000, 'big')),ct),self.certificate.public_bytes(encoding=serialization.Encoding.PEM))
+        
+        elif self.msg_cnt==1:
+            #Client Signature Validation
+            nonce = (mc.unpair(msg)[0])[:12]
+            serverEncryptedSignature = (mc.unpair(msg)[0])[12:]
+            aad = b""
+            serverDecryptedSignature = self.aesgcm.decrypt(nonce, serverEncryptedSignature, aad)
 
-        nonce = os.urandom(12)
+            client_certificate=x509.load_pem_x509_certificate((mc.unpair(msg)[1]), default_backend())
 
-        ct = nonce + self.aesgcm.encrypt(nonce, new_msg, aad)
-    
-        return ct,self.msg_cnt if len(new_msg)>0 else None,self.msg_cnt
+            # Server Certificate Validation
+            mc.valida_cert(client_certificate)
+            
+            try:
+                self.peerRSA_public_key.verify(
+                    serverDecryptedSignature,
+                    mc.mkpair(self.peerGX.to_bytes(1000, 'big'),self.gy.to_bytes(1000, 'big')),
+                    padding.PSS(
+                        mgf=padding.MGF1(hashes.SHA256()),
+                        salt_length=padding.PSS.MAX_LENGTH
+                    ),
+                    hashes.SHA256()
+                )
+            except InvalidSignature:
+                print("Validação da assinatura digital não efetuada!!!")
+                return
+            
+            return self._process_message(None)
+        
+
+    def _process_message(self, msg):
+        self.msg_cnt+=1
+
+        if msg:
+            nonce = msg[:12]
+            informacao = msg[12:]
+            aad = b""
+            decrypted_msg = self.aesgcm.decrypt(nonce, informacao, aad)
+            print('%d : %r' % (self.id,decrypted_msg.decode()))
+
+            new_msg = decrypted_msg.upper()
+
+            nonce = os.urandom(12)
+            ct = nonce + self.aesgcm.encrypt(nonce, new_msg, aad)
+            return ct if len(new_msg) > 0 else None
+        
+        else: ##Caso em que acontece a validacao da signature do cliente
+            return b"Handshake terminado com sucesso!!!"
 
 
 async def handle_echo(reader, writer):
@@ -86,8 +133,8 @@ async def handle_echo(reader, writer):
     while True:
         if not data: continue
         if data[:1]==b'\n': break
-        data,msg_cnt = srvwrk.process(data)
-        if (not data) and msg_cnt>2: break
+        data = srvwrk.process(data)
+        if not data: break
         writer.write(data)
         await writer.drain()
         data = await reader.read(max_msg_size)
@@ -105,10 +152,10 @@ def run_server():
         loop.run_forever()
     except KeyboardInterrupt:
         pass
-
-    server.close()
+    server.close()          
     loop.run_until_complete(server.wait_closed())
     loop.close()
     print('\nFINISHED!')
+
 
 run_server()
