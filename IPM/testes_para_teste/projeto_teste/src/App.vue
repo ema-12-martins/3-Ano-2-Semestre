4e1504c748e43212722bca80656809b69fe47232
ema-12-martins <a97678@alunos.uminho.pt>
2024-05-21 23:19:40 +0100

    coisas

---
+++ IPM/testes_para_teste/projeto_teste/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Pagina_Teste msg="Vais escrever isto"/>
+</template>
+
+<script>
+import Pagina_Teste from './components/Pagina_Teste.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Pagina_Teste
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ IPM/testes_para_teste/projeto_teste/src/components/Pagina_Teste.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-1">
+    <h1>{{ msg }}</h1>
+    <p>Este é só um texto exemplo para testar coisas em CSS.</p>
+    <p>Não se percebe porque temos de programar CSS no papel mas faz parte.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pagina_Teste',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container-1{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
+  background-color: aqua;
+}
+</style>
